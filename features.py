e379ed11e253d0a99b8b4dbb390032948286c46c
amorpfr <aaamorus@hotmail.com>
2017-11-29 11:20:37 +0100

    test pickle

--- features.py
+++ features.py
@@ -5,6 +5,8 @@ import pprint as pp
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import mahotas as mt
+
 
 def thresholding(image):
     imthr = np.where(image > np.mean(image),0.,1.0)
@@ -14,11 +16,19 @@ def thresholding(image):
     labels = labels.astype(int)
     return labels
 
-if __name__ == "__main__":
-    path = 'C:/Users/daniel/Documents/AML Kaggle/images.pkl'
-    images  = pd.read_pickle(path)
-    images['mean'] = images['image_matrix'].apply(thresholding)
+def extract_features(image):
+    # calculate haralick texture features for 4 types of adjacency
+    textures = mt.features.haralick(image)
+
+    # take the mean of it and return it
+    ht_mean = textures.mean(axis=0)
+    return ht_mean
     
-    plt.figure(figsize = (12,3))
-    sub = plt.subplot(1,4,1)
-    plt.imshow(images['mean'].loc[1], cmap='gray')
+if __name__ == "__main__":
+    path = 'images.pkl'
+    images  = pd.read_pickle(path)
+    images['clean'] = images['image_matrix'].apply(thresholding)
+    image = images['image_matrix'].iloc[0] 
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    features = extract_features(image)
